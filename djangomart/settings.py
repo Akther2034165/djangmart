54d4bbdcddb5e6e07437e513702c300b3945ffab
Md Akther Hosen <dev.aktherhosen@gmail.com>
2023-10-10 18:49:14 +0600

    update

--- djangomart/settings.py
+++ djangomart/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@qk%gb*solwbh=a9i)jyf!z#n+u(u9(7_py*=u1**$5udri#wb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
